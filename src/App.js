68bb6916b9668327df940a142bc36479c70a5137
chamod-gamage <41309709+chamod-gamage@users.noreply.github.com>
2020-06-19 23:09:06 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {Layout} from './components/Layout.js'
 // import {NavigationBar} from './Home.js'
 // const resume = require('./assets/chamod-resume.pdf')
 
-function initializeReactGA() {
-  ReactGA.initialize('UA-170137058-1');
-  ReactGA.pageview('/homepage');
-}
 
 const App = () => {
+  
+  useEffect(() => {
+    ReactGA.initialize('UA-170137058-1');
+    ReactGA.pageview('/homepage');
+  }, [])
+  
   const [selected, setSelected] = useState(0)
     return (
       <React.Fragment>
