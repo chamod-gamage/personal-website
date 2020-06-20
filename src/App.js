135fb6351ca9cf64ac01873741b254741aadd276
chamod-gamage <41309709+chamod-gamage@users.noreply.github.com>
2020-06-19 23:09:24 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import {Home} from './Home.js'
 import ReactGA from 'react-ga'
