77ce11c3adb1da4bb4b8e8f5053e8d4baaef388f
Chamod Gamage <chamodgamage26@gmail.com>
2023-02-10 05:30:00 +0800

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -178,7 +178,14 @@ export const Home = () => {
         <Row>
           <Col lg={3} style={{ textAlign: "center" }}>
             <div style={{ height: 20 }} />
-            <Image src={clicks >= 10 ? panda : me} className="profile" onClick={() => { setClicks(clicks + 1) }} roundedCircle />
+            <Image
+              src={clicks >= 10 ? panda : me}
+              className="profile"
+              onClick={() => {
+                setClicks(clicks + 1);
+              }}
+              roundedCircle
+            />
             <div style={{ height: 10 }} />
           </Col>
           {/* <Col md = {1}/> */}
@@ -645,7 +652,7 @@ export const Home = () => {
       ],
       image: uwcribs,
       buttonText: "Use the product",
-      link: "https://donationhub.pregnancycentre.ca/",
+      link: "https://uwcribs.com/",
       description: `Finding housing every term is a universally lamented experience amongst post-secondary students, due to the disjointed, time-consuming, and uncurated nature of the process. UW Cribs sources posts from sellers on commonly used platforms like Facebook Marketplace and provides them to users in a centralized user interface, with filtering functionality, allowing students to curate posts based on relevant data categories such as budget, bedrooms, bathrooms, and season.
       The project includes a Turborepo monorepo consisting of two Node.js servers and a Next.js web application written in TypeScript, internal tooling built in Retool, and a MongoDB database.`,
     },
